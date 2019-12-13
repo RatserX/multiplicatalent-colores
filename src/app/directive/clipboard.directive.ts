be7fcfa90020d5a1667ef21bd56104b460ea2014
Miguel <12604047+RatserX@users.noreply.github.com>
2019-12-12 22:27:25 -0500

    Updated package-lock, clipboard directive

--- src/app/directive/clipboard.directive.ts
+++ src/app/directive/clipboard.directive.ts
@@ -5,17 +5,17 @@ import { Directive, Input, Output, EventEmitter, HostListener } from '@angular/c
 })
 export class ClipboardDirective {
 
-  @Input("payload")
+  @Input('payload')
   payload: string
 
-  @Output("payloadCopied")
+  @Output('payloadCopied')
   payloadCopied: EventEmitter<string>
 
   constructor() {
     this.payloadCopied = new EventEmitter<string>();
   }
 
-  @HostListener("click", ["$event"])
+  @HostListener('click', ['$event'])
   public onClick(event: MouseEvent) {
     event.preventDefault();
 
@@ -23,17 +23,45 @@ export class ClipboardDirective {
       return;
     }
 
-    let listener = (e: ClipboardEvent) => {
-      let clipboardData = e.clipboardData || window["clipboardData"];
+    this.copy(this.payload);
 
-      clipboardData.setData("text", this.payload);
+    // This code below does not works on Safari or iOS devices
+    /*let listener = (e: ClipboardEvent) => {
+      let clipboardData = e.clipboardData || window['clipboardData'];
+
+      clipboardData.setData('text', this.payload);
       e.preventDefault();
       this.payloadCopied.emit(this.payload);
     }
 
-    document.addEventListener("copy", listener, false);
-    document.execCommand("copy");
-    document.removeEventListener("copy", listener, false);
+    document.addEventListener('copy', listener, false);
+    document.execCommand('copy');
+    document.removeEventListener('copy', listener, false);*/
+  }
+
+  private copy(value: string) {
+    let textArea : HTMLInputElement = (<HTMLInputElement>document.createElement('textArea'));
+    textArea.value = value;
+
+    document.body.appendChild(textArea);
+
+    let isIOS = navigator.userAgent.match(/ipad|iphone/i);
+
+    if (isIOS) {
+      let range = document.createRange();
+      let selection = window.getSelection();
+
+      range.selectNodeContents(textArea);
+      selection.removeAllRanges();
+      selection.addRange(range);
+      textArea.setSelectionRange(0, value.length);
+    } else {
+      textArea.select();
+    }
+    
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+    this.payloadCopied.emit(value);
   }
 
 }
